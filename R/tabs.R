99fd49a7df2bf63d704d02dd0eeb4728741e96bc
fawda123 <mbafs2012@gmail.com>
2022-08-22 16:45:25 -0400

    inter and supratidal habitat change analysis table done

--- R/tabs.R
+++ R/tabs.R
@@ -1,5 +1,6 @@
 library(flextable)
 library(tidyverse)
+library(here)
 
 # subtidal habitat change ---------------------------------------------------------------------
 
@@ -51,4 +52,58 @@ save(subttab, file = here('tabs/subttab.RData'))
 
 # inter/suptratidal habitat change ------------------------------------------------------------
 
+data(acres)
 
+losscol <- 'red'
+gaincol <- 'green'
+
+# format for the table
+totab <- acres %>%
+  rename(
+    `Habitat Type` = HMPU_TARGETS,
+    yr = name,
+    Hectares = Acres
+  ) %>%
+  filter(yr <= 2017) %>%
+  mutate(
+    Stratum = case_when(
+      `Habitat Type` %in% c('Mangrove Forests', 'Salt Barrens', 'Salt Marshes') ~ 'Intertidal',
+      T ~ 'Supratidal'
+    ),
+    Hectares = Hectares / 2.471
+  ) %>%
+  tidyr::pivot_wider(names_from = yr, values_from = Hectares) %>%
+  mutate(
+    `1990 to 2017` = `2017` - `1990`,
+    `% change` = 100 * (`2017` - `1990`) / `1990`,
+    chgcol = case_when(
+      `% change` < 0 & `Habitat Type` != 'Developed' ~ 0,
+      `% change` >= 0 & `Habitat Type` != 'Developed' ~ 1,
+      `% change` < 0 & `Habitat Type` == 'Developed' ~ 1,
+      `% change` >= 0 & `Habitat Type` == 'Developed' ~ 0,
+    )
+  ) %>%
+  arrange(Stratum, `Habitat Type`) %>%
+  as_grouped_data(groups = 'Stratum')
+
+thm <- function(x){
+  flextable::colformat_double(x, digits = 0, na_str = '') %>%
+    flextable::align(align = 'center', part = 'all') %>%
+    flextable::align(align = 'left', j = 1, part = 'all') %>%
+    flextable::border_inner() %>%
+    flextable::width(width = 0.7, j = 1) %>%
+    flextable::width(width = 5.8 / (ncol_keys(.) - 1), j = 2:ncol_keys(.)) %>%
+    flextable::padding(padding = 0, part = 'all') %>%
+    flextable::fontsize(size = 7, part = 'all') %>%
+    flextable::padding(padding = 0, part = 'all') %>%
+    flextable::fontsize(size = 7, part = 'all')
+}
+
+# table
+acretab <- flextable::flextable(totab, col_keys = grep('^chgcol$', names(totab), value = T, invert = T)) %>%
+  flextable::bg(i = ~ `chgcol` == 0, j = '% change', bg = losscol) %>%
+  flextable::bg(i = ~ `chgcol` == 1, j = '% change', bg = gaincol) %>%
+  thm
+
+# acretab
+save(acretab, file = here('tabs/acretab.RData'))
